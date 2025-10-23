5e7b32493b7dcfa2ee6178e1b41a4cc67113af65
Stanislav Kaliuzhnyi <stanislavkaliuzhnyi@Stanislavs-MacBook-Pro.local>
2025-10-22 23:02:21 -0700

    wrap with EquatableView

--- Swift3D/Sources/Swift3D/Metal/MetalSwiftView.swift
+++ Swift3D/Sources/Swift3D/Metal/MetalSwiftView.swift
@@ -8,22 +8,53 @@
 import UIKit
 import SwiftUI
 
-public struct Swift3DView: UIViewRepresentable {
+// Swift3DView + EquatableView
+
+public typealias Swift3DView = EquatableView<FreeSwift3DView>
+
+public extension Swift3DView {
+  init(
+    preferredFps: Int = 30,
+    updateLoop: ((_ deltaTime: Double) -> Void)? = nil,
+    @SceneBuilder _ content: @escaping () -> any Node
+  ) {
+    self.init(
+      content: FreeSwift3DView(
+        preferredFps: preferredFps,
+        updateLoop: updateLoop,
+        content
+      )
+    )
+  }
+}
+
+// Prefer Swift3DView over inequtable view
+
+public struct FreeSwift3DView: UIViewRepresentable, Equatable {
+  let id = UUID()
   let updateLoop: ((_ deltaTime: Double) -> Void)?
   let preferredFps: Int
   let content: () -> any Node
   
   public init(
-    preferredFps: Int = 30,
-    updateLoop: ((_ deltaTime: Double) -> Void)? = nil,
+    preferredFps: Int,
+    updateLoop: ((_ deltaTime: Double) -> Void)?,
     @SceneBuilder _ content: @escaping () -> any Node
   ) {
     self.updateLoop = updateLoop
     self.preferredFps = preferredFps
     self.content = content
   }
+  
+  // MARK: - Equatable
+  
+  public static func == (lhs: FreeSwift3DView, rhs: FreeSwift3DView) -> Bool {
+    lhs.id == rhs.id && lhs.preferredFps == rhs.preferredFps
+  }
+  
+  // MARK: - UIViewRepresentable
 
-  public func makeUIView(context: Context) -> UIView {
+  public func makeUIView(context: Context) -> MetalView {
     do {
       return try MetalView(
         preferredFps: preferredFps,
@@ -31,14 +62,12 @@ public struct Swift3DView: UIViewRepresentable {
         contentFactory: content
       )
     } catch {
-      assertionFailure(String(describing: error))
-      
-      let errorLabel = UILabel()
-      errorLabel.text = String(describing: error)
-      errorLabel.sizeToFit()
-      return errorLabel
+      fatalError(String(describing: error))
     }
   }
 
-  public func updateUIView(_ uiView: UIView, context: Context) {}
+  public func updateUIView(_ uiView: MetalView, context: Context) {
+    // TODO: pass preferredFps
+    print("updateUIView \(context)")
+  }
 }
